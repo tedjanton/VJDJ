28f8d3d8ec2f8658b0ae4163ef277471b9d00309
Ted Anton <tedjanton@gmail.com>
2021-03-30 13:18:19 -0500

    added background color and circles, imported font

--- react-app/src/components/Landing/index.js
+++ react-app/src/components/Landing/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import "./Landing.css";
 
 
 const Landing = ({ authenticated }) => {
 
 
   return (
-    <>
+    <div className="landing-background-container">
       <h1>Landing</h1>
-    </>
+    </div>
   )
 }
 

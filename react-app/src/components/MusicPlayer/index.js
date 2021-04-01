33901d3305e11354f4fca4d678518c9a1d22d7f5
Ted Anton <tedjanton@gmail.com>
2021-04-01 13:13:58 -0500

    play pause skip prev all working in player

--- react-app/src/components/MusicPlayer/index.js
+++ react-app/src/components/MusicPlayer/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Controls from "./Controls";
 import chromatica from '../../images/chromatica.jpeg';
+import future_nostalgia from '../../images/future_nostalgia.jpeg';
 import './MusicPlayer.css';
 
 
@@ -10,8 +11,13 @@ const MusicPlayer = ({}) => {
     artist: "Lady Gaga",
     art: chromatica,
     audio_src: 'https://vjdj.s3.amazonaws.com/music/lady-gaga/Chromatica/03+Stupid+Love.m4a'
+  }, {
+    title: "Don't Start Now",
+    artist: "Dua Lipa",
+    art: future_nostalgia,
+    audio_src: 'https://vjdj.s3.amazonaws.com/music/dua-lipa/future-nostalgia/02-dua_lipa-dont_start_now.mp3'
   }]
-  const [trackIdx, setTrackIds] = useState(0);
+  const [trackIdx, setTrackIdx] = useState(0);
   const [trackProgress, setTrackProgress] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -23,12 +29,50 @@ const MusicPlayer = ({}) => {
 
   const { duration } = audioRef.current;
 
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  });
+
+  useEffect(() => {
+    return () => {
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    audioRef.current.pause();
+
+    audioRef.current = new Audio(audio_src);
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      // startTimer();
+    } else {
+      isReady.current = true;
+    }
+  }, [trackIdx])
+
   const toPrevTrack = () => {
-    console.log("go to prev")
+    if (trackIdx - 1 < 0) {
+      setTrackIdx(tracks.length - 1);
+    } else {
+      setTrackIdx(trackIdx - 1);
+    }
   }
 
   const toNextTrack = () => {
-    console.log("go to next")
+    if (trackIdx < tracks.length - 1) {
+      setTrackIdx(trackIdx + 1);
+    } else {
+      setTrackIdx(0)
+    }
   }
 
 

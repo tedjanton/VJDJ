0417d9c27d4acfd470af6aee7cf820f5fd299db8
Ted Anton <tedjanton@gmail.com>
2021-04-02 14:42:25 -0500

    added hover on track box component

--- react-app/src/components/TrackBox/index.js
+++ react-app/src/components/TrackBox/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TrackBox.css';
 
 
 const TrackBox = ({ track }) => {
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseHover = () => {
+    setIsHover(!isHover);
+  }
 
   return (
-    <div className="tb-container">
+    <div
+      className="tb-container"
+      onMouseEnter={handleMouseHover}
+      onMouseLeave={handleMouseHover}>
       <div className="tb-img">
         <img src={track.album.art_src} alt={track.title} />
+        {isHover && (
+          <button className="tb-play-button">
+            <i className="tb fas fa-play" />
+          </button>
+        )}
       </div>
       <div className="tb-title">
         <span>{track.title}</span>

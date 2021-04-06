d0995afcc9a05e0e5b26e5638525cd7deb78893a
Ted Anton <tedjanton@gmail.com>
2021-04-05 20:40:59 -0500

    fleshed out login page with styling

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
+import google from '../../images/google.png';
 import "./LoginForm.css";
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+}
 
 const LoginForm = ({ setNav, authenticated, setAuthenticated }) => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [windowDims, setWindowDims] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDims(getWindowDimensions());
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
 
   useEffect(() => {
     setNav(false)
@@ -30,35 +46,90 @@ const LoginForm = ({ setNav, authenticated, setAuthenticated }) => {
   }
 
   return (
-    <div className="login-container">
-      <form onSubmit={onLogin}>
-        <div>
-          {errors.map((error) => (
-            <div>{error}</div>
-            ))}
+    <div
+      className="login-page"
+      style={{ width: windowDims.width, height: windowDims.height}}
+    >
+      <div className="login-container">
+        <div className="login-logo">
+          <i className="fab fa-spotify login" />
+          <div className="login-logo-title">
+            <span className="v login">V</span>
+            <span className="j login">J</span>
+            <span className="d login">D</span>
+            <span className="j login">J</span>
+          </div>
         </div>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+        <div className="login-instructions">
+          <p>To continue, log in to VjDj.</p>
+        </div>
+        <div className="login-fb-auth">
+          <button>
+            <i className="fab fa-facebook" />
+            CONTINUE WITH FACEBOOK
+          </button>
+        </div>
+        <div className="login-apple-auth">
+          <button>
+            <i className="fab fa-apple" />
+            CONTINUE WITH APPLE
+          </button>
+        </div>
+        <div className="login-google-auth">
+          <button>
+            <img className="google-logo" src={google} alt="google" />
+            CONTINUE WITH GOOGLE
+          </button>
+        </div>
+        <div className="login-or-separator">
+          <div className="login-or-line"></div>
+          <div className="login-or">OR</div>
+          <div className="login-or-line"></div>
+        </div>
+        <form className="login-form" onSubmit={onLogin}>
+          <div className="login-form-errors">
+            {errors.map((error) => (
+              <div>{error}</div>
+              ))}
+          </div>
+          <div className="login-email">
+            <label htmlFor='email'>Email address</label>
+            <input
+              name='email'
+              type='text'
+              placeholder='Email address'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+          </div>
+          <div className="login-password">
+            <label htmlFor='password'>Password</label>
+            <input
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            name='password'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
-          <button type='submit'>Login</button>
-        </div>
-      </form>
+          </div>
+          <div className="login-forgot-password">
+            <p>Forgot your password?</p>
+          </div>
+          <div className="login-remember-button-container">
+            <div className="login-remember-container">
+              <input name="remember" type="checkbox" />
+              <label for="remember">Remember me</label>
+            </div>
+            <div className="login-submit">
+              <button type="submit">LOG IN</button>
+            </div>
+          </div>
+          <div className="login-no-account">
+            <p>Don't have an account?</p>
+            <button onClick={() => history.push("/sign-up")}>SIGN UP FOR VJDJ</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };

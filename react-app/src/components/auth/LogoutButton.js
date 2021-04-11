78d4be1245d46e0af19162b780244f5a9d513255
Ted Anton <tedjanton@gmail.com>
2021-04-11 13:43:27 -0500

    fixed autoplay from logout to login

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router';
+import { AppWithContext } from '../../App';
 import { logout } from '../../store/session';
 
 const LogoutButton = ({setAuthenticated, setMenu}) => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const { setIsPlaying } = useContext(AppWithContext);
   const onLogout = async (e) => {
     setMenu(false);
     setAuthenticated(false);
     await dispatch(logout());
-    // window.location.reload();
+    setIsPlaying(false);
     return history.push("/");
   };
 
